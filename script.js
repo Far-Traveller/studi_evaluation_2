4bd5c893b2aca950f3634ec1321f828ab57fbe0f
Far-Traveller <carriere.aurore@orange.fr>
2022-03-29 18:25:19 +0200

    function nextRound added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let firstPlayerName = document.getElementById('first-player-name');
 let secondPlayerName = document.getElementById('second-player-name');
 
 // Current player
-let playerBackground = document.getElementsById('player-background');
+let playerBackground = document.getElementById('player-background');
 let firstPlayerTurn = document.getElementById('first-player-turn');
 let secondPlayerTurn = document.getElementById('second-player-turn');
 
@@ -33,7 +33,49 @@ const dice6Faces = [
     `<div class="dice face-6"><div class="dice-column"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div><div class="dice-column"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div></div>`,
 ];
 
+// Var for the game
+let activePlayer;
+let activePlayerCurrent = 0;
+let activePlayerTotal = 0;
+
+let isGamePlaying = true;
+let player1;
+let player2;
+
 
 /**
  * Functions
  */
+
+// play : restart game
+
+
+// roll
+
+
+// hold
+
+// next round
+function nextRound() {
+    if (activePlayer === player1){
+        activePlayer = player2;
+
+        playerBackground.classList.remove('turn-player-1');
+        firstPlayerTurn.classList.remove('dot-current-player');
+        firstPlayerName.classList.remove('font-current-player');
+
+        playerBackground.classList.add('turn-player-2');
+        secondPlayerTurn.classList.add('dot-current-player');
+        secondPlayerName.classList.add('font-current-player');
+    } else {
+        activePlayer = player1;
+        
+        playerBackground.classList.remove('turn-player-2');
+        secondPlayerTurn.classList.remove('dot-current-player');
+        secondPlayerName.classList.remove('font-current-player');
+
+        playerBackground.classList.add('turn-player-1');
+        firstPlayerTurn.classList.add('dot-current-player');
+        firstPlayerName.classList.add('font-current-player');
+    }
+}
